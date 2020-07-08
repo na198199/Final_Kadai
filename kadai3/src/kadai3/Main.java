d7c82e14b4035541583904ae49d2b3f1f5485447
kojimanaoko <kojimanaoko@localhost>
2020-07-08 14:28:50 +0900

    とりあえずPush

---
+++ kadai3/src/kadai3/Main.java
@@ -0,0 +1,42 @@
+//G18914 小島尚子
+//Mainクラス
+
+package kadai3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyAccount acc  = new MyAccount();
+		
+		acc.setAccNum(14);
+		acc.setPass(18914);
+		acc.setBalance(18914*10);
+		
+		System.out.println("Account Number: "+acc.getAccNum());
+		System.out.println("Password: "+acc.getPass());
+		System.out.println("Balance: "+acc.getBalance()+" JPY");
+	
+		
+		
+		TestATM atm = new TestATM(acc);
+		
+		int deposit = 2500;//入金額
+		System.out.println("Start deposit: " +deposit + " JPY");
+		 atm.deposit(deposit);
+		 atm.showCurrentBalance();
+		  	 
+		int withdraw = 1100;
+		System.out.println("Start withdraw: " +withdraw + " JPY");
+		atm.withdraw(withdraw);
+		atm.showCurrentBalance();
+		
+		withdraw = 1000000000;
+		System.out.println("Start withdraw: " + withdraw + " JPY");
+		atm.withdraw(withdraw);
+		atm.showCurrentBalance();
+		
+		
+	}
+
+}
---
+++ kadai3/src/kadai3/MyAccount.java
@@ -0,0 +1,42 @@
+//G18914　小島尚子
+//銀行機能を持ったプログラム
+
+package kadai3;
+
+public class MyAccount {
+	private int m_accNum;
+	private int m_password;
+	private int m_balance;
+	
+
+	public MyAccount(){
+		
+	}
+		
+		public int getAccNum() {//口座番号
+			return this.m_accNum;
+			}
+		public void setAccNum(int accnum){
+			this.m_accNum = accnum;
+			}
+		
+		public int getPass(){//暗証番号
+		    return this.m_password;
+			}
+		public void setPass(int pass){
+			this.m_password = pass;
+			}
+		
+		public int getBalance(){//残高
+			return  this.m_balance;
+			
+			}
+		public void setBalance(int balance){
+			this.m_balance = balance;
+			}
+		
+
+}
+
+
+
